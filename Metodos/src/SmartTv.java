2f5de7f842291ba9f7f0e78cca57d753baa68da6
belliws <belliws@hotmail.com>
2022-10-26 23:25:05 -0300

    aula metodos

---
+++ Metodos/src/SmartTv.java
@@ -0,0 +1,39 @@
+public class SmartTv {
+    boolean ligada = false;
+    int canal = 1;
+    int volume = 25;
+
+    public void aumentarCanal(){
+        canal++;
+        System.out.println("Canal atual: " + canal);
+    }
+    
+    public void diminuirCanal(){
+        canal--;
+        System.out.println("Canal atual: " + canal);
+    }
+
+    public void mudarCanal(int novoCanal){
+        canal = novoCanal;
+        System.out.println("Canal atual: " + canal);
+    }
+
+    public void aumentarVolume(){
+        //a mesma coisa que volume = volume + 1;
+        volume++;
+        System.out.println("Aumentando volume para: " + volume);
+    } 
+
+    public void diminuirVolume(){
+        //a mesma coisa que volume = volume - 1;
+        volume--;
+        System.out.println("Diminuindo volume para: " + volume);
+    } 
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+}
