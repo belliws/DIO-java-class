e29b75b569ca7cab19ef962967e3ca6acf18fecd
Rodrigo Moreira <115381618+belliws@users.noreply.github.com>
2022-11-09 21:03:01 -0300

    Create Desafio.java

---
+++ Desafio.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class Desafio {
+
+    public static void main(String[] args) {
+        
+     Scanner leitor = new Scanner (System.in);
+        
+        System.out.println("Digite o primeiro número: ");
+        int a = leitor.nextInt();
+        
+        System.out.println("Digite o segundo número: ");
+        int b = leitor.nextInt();
+        
+      if (a==b){
+        
+        System.out.println("Sao iguais!");
+      }
+      
+        else System.out.println("Nao sao iguais!");
+      
+
+        //TODO: Imprima se os valores numéricos passados são iguais ou não.
+
+    }
+}
